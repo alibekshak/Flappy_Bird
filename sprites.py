912372d3c9ca57ace285896042db6d5f0cd8a8b6
alibekshak <alibekshakiro1@gmail.com>
2023-05-11 11:03:53 +0600

    creat ground

--- sprites.py
+++ sprites.py
@@ -1,6 +1,5 @@
-from typing import Any
+
 import pygame
-from pygame.sprite import _Group
 from settings import *
 
 class Background(pygame.sprite.Sprite):
@@ -29,8 +28,17 @@ class Background(pygame.sprite.Sprite):
 class Ground(pygame.sprite.Sprite):
     def __init__(self, groups, sclae_factor):
         super().__init__(groups)
-        ground_surf = pygame.image.load("").convert_alpha()
+        ground_surf = pygame.image.load("assets/sprites/ground.png").convert_alpha()
         self.image = pygame.transform.scale(ground_surf, pygame.math.Vector2(ground_surf.get_size()) * sclae_factor)
 
         # position
         self.rect = self.image.get_rect(bottomleft = (0, HEIGHT))
+        self.position = pygame.math.Vector2(self.rect.topleft)
+
+    def update(self, dt):   
+        self.position.x -= 300 * dt
+        if self.rect.centerx <= 0:
+            self.position.x = 0
+
+        self.rect.x = round(self.position.x)
+        
